a9eaf5c86ab49be0c7f99d07fe04e63463631af0
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-03 21:01:45 +0900

    re06

--- main.c
+++ main.c
@@ -3,21 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	unsigned int x;
-	int b;
-	printf("input a number:");
-	scanf("%ui",&x);
-	
-	for (b=0;x!=0; x>>=1)//->x=x>>1
-	{
-		if (x&1)
-		{
-			b++;
-		}
-	}
-	printf("The result is: %i\n",b);
-	
-	
+
+int main() {
+    int seconds, hours, minutes;
+
+    printf("input a seconds: ");
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600; 
+    seconds %= 3600; 
+
+    minutes = seconds / 60; 
+    seconds %= 60;
+
+    printf("시간(시:분:초): %02d:%02d:%02d\n", hours, minutes, seconds);
+
     return 0;
 }
