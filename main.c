157c6ee252713ef7f0ada5a35c57545521ef1671
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-03 20:49:58 +0900

    06

--- main.c
+++ main.c
@@ -4,28 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
-	printf("input the year: ");
-	scanf("%i",&a);
+	unsigned int x;
+	int b;
+	printf("input a number:");
+	scanf("%ui",&x);
 	
-	if ( (a%4==0 && a%100!=0)||(a%400==0))
+	for (b=0;x!=0; x>>=1)//->x=x>>1
 	{
-	
-		printf("%i is leap year.\n",a);
+		if (x&1)
+		{
+			b++;
+		}
 	}
-	else
-	{
+	printf("The result is: %i\n",b);
 	
-	printf("%i is not leap year. \n",a);
-	}	
-	
-	 
-	
-	//1. 4로 나누어 떨어져야함
-	//2. 100으로 나누어 떨어지지 않아야함.
-	//3, 위를 만족안해도 400으로 나누어 떨어짐.
-	 
-	
-
     return 0;
 }
